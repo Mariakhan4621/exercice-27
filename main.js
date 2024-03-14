a4aeedd7c119c324abc56c79673913e8f4d215b3
Mariakhan4621 <160171512+Mariakhan4621@users.noreply.github.com>
2024-03-14 20:17:30 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+var alient_color = "green";
+if (alient_color == "green") {
+    console.log("5 points");
+}
+else if (alient_color == "yellow") {
+    console.log("10 points");
+}
+else {
+    console.log("15 point");
+}
---
+++ main.ts
@@ -0,0 +1,11 @@
+let alient_color: string = "green"
+
+if (alient_color == "green") {
+    console.log("5 points")
+} else if (alient_color == "yellow") {
+    console.log("10 points")
+}else {
+    console.log("15 point")
+
+}
+    
